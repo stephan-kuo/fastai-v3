39e7da6d9a3d1e1e3c8451294e6dcb77c8b93a6c
stephan-kuo <50568631+stephan-kuo@users.noreply.github.com>
2019-11-29 08:05:11 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=13WPzv98TuFRCApKjI8h-r7wFg5N6XoGk'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['土半夏', '姑婆芋', '毛忍冬', '血桐', '賣子木', '雞蛋花', '魚腥草']
 path = Path(__file__).parent
 
 app = Starlette()
